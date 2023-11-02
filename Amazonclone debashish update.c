06278df3b9aeca8d37f9682d6a47ac6212ea572a
DebNayak05 <143987174+DebNayak05@users.noreply.github.com>
2023-11-03 02:19:46 +0530

    Create Amazonclone debashish update.c
    Ye mera wala update hai. Kal tak pura kar liyo plzzzz

---
+++ Amazonclone debashish update.c
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Amazon</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" href="favicon.ico">
+</head>
+<body>  
+    <header>
+        <div class="navbar">
+            <div class="nav-logo border">
+                <div class="logo"></div>
+            </div>
+            <div class="nav-address border">
+                <p class="address1">Deliver to</p>
+                <div class="add-icon">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p class="address2  ">India</p>
+                </div>
+            </div>
+            <div class="nav-search">
+                <select class="search-select">
+                    <option>All</option>
+                    <option>All departments</option>
+                    <option>Arts & crafts</option>
+                    <option>Automotive</option>
+                    <option>Baby</option>
+                    <option>Beauty & personal care</option>
+                    <option>Books</option>
+                    <option>Boys' fashion</option>
+                    <option>Computers</option>
+                    <option>Deals</option>
+                    <option>Digital music</option>
+                    <option>Electronics</option>
+                    <option>Girls' fashion</option>
+                    <option>Health & Household</option>
+                </select>
+                <input class="search-input" placeholder="Search Amazon">
+                <div class="search-icon">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+            <div class="nav-signin border">
+                <p ><span>Hello, Sign in</span></p>
+                <p class="nav-second">Account & Lists</p>
+            </div>
+            <div class="nav-return border">
+                <p ><span>Returns</span></p>
+                <p class="nav-second">& Orders</p>
+            </div>
+            <div class="nav-cart border">
+                <i class="fa-solid fa-cart-shopping"></i>
+                Cart
+            </div>
+        </div>
+        <div class="panel">
+            <div class="panel-all border">
+                <i class="fa-solid fa-bars"></i>
+                <div class="dropdown">
+                    <button class="dropbtn">All</button>
+                    <div class="dropdown-content">
+                        <a href="#">Digital content & devices</a>
+                        <a href="#">Shop by department</a>
+                        <a href="#">Programs & features</a>
+                        <a href="#">Help & settings</a>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="panel-ops">
+                <p class="border">Today's Deal</p>
+                <p class="border">Customer Service</p>
+                <p class="border">Registry</p>
+                <p class="border">Gift Cards</p>
+                <p class="border">Sell</p>
+            </div>
+            <div class="panel-deals border">
+                Shops Deals in Electronics
+            </div>
+        </div>
+    </header>
+    <div class="hero-section">
+        <div class="hero-msg">
+            <p>You are on Amazon.com. You can also shop on Amazon India for millions of products with fast local delivery. <a href="https://www.amazon.in/">Click Here to Go to Amazon.in</a></p>
+        </div>
+    </div>
+    <div class="content">
+        <div class="box1 box">
+            <div class="box-content">
+                <h2>Health and Personal Care</h2>
+                <div class="box-img" style="background-image: url('box1_image.jpg');"></div>
+                <p>Shop More</p>
+            </div>
+        </div>
+        <div class="box2 box">Box2</div>
+        <div class="box3 box">Box3</div>
+        <div class="box4 box">Box4</div>
+    </div>
+</body>
+</html>
